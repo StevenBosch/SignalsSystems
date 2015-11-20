7762634e65c2e4838e94fd1e1b4eeca94546bfeb
Maikel School <m.l.c.withagen@student.rug.nl>
2015-11-20 11:59:18 +0100

    woei

---
+++ lab1/code/gensinusoid.m
@@ -0,0 +1,10 @@
+function [ans] = gensinusoid(A,f,phi,t1,t2,Fs)
+	# The sinusoid function is: y(n)=Asin(2π f n+phi)
+	totalTime = t2-t1
+	nSamples = totalTime * Fs
+	ans = zeros(1, nSamples)
+	for i = 1:nSamples
+		t = t1 + (i-1)*1/Fs
+		ans(i) = A * cos (2*pi  *f*t + phi) 	
+	endfor
+end
