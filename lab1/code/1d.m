9fef396605acc9f6124152e1e1b106f590496fde
Maleco <maikel.withagen@gmail.com>
2015-12-01 13:55:44 +0100

    spul

--- lab1/code/1d.m
+++ lab1/code/1d.m
@@ -1,5 +1,8 @@
+# Generate both signals
 x1 = gensinusoid(5, 400, 0, 0, 0.5, 8000)
 x2 = gensinusoid(5, 400, 10.5*pi, 0, 0.5, 8000)
+
+# Plot the sum of the two signals
 
 plot(x1(1:200)+x2(1:200))
 xlabel("Sample")
@@ -8,7 +11,7 @@ title ("The first 200 samples of the signal x1(t) and x2(t) added")
 legend ({"x1(t) + x2(t)"});
 print -deps ../report/plot4A.eps
 
-# Now we can generate the proper function y(t) with our sumsinusoid function
+# Generate the proper function y(t) with our sumsinusoid function
 [A, f, phi] = sumsinusoid(5,400,0,5,10.5*pi)
 y = gensinusoid(A, f, phi, 0, 0.5, 8000)
 
--- lab1/code/1e.m
+++ lab1/code/1e.m
@@ -4,6 +4,6 @@ x3 = gensinusoid(1,800,0,0,0.5,8000)
 plot(x1(1:200)+x3(1:200))
 xlabel("Sample")
 ylabel("Amplitude (A)")
-title ("The first 200 samples of the signal x1(t) and x2(t) added")
-legend ({"x1(t) + x2(t)"});
+title ("The first 200 samples of x1(t) + x3(t)")
+legend ({"x1(t) + x3(t)"});
 print -deps ../report/plot1E.eps
