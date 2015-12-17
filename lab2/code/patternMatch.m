f4c00477b15a4359d95eac7e930ca71ab3d6ffe1
Maleco <maikel.withagen@gmail.com>
2015-12-17 14:34:55 +0100

    added patternMatch function

---
+++ lab2/code/patternMatch.m
@@ -0,0 +1,33 @@
+function ans = patternMatch(x, h)
+  if (length(h) > length(x))
+    error('pearson(x,y): x and y are not of the same shape');
+  end
+
+  signal = x;
+  mask = h;
+
+  # Calculate cumsum for later calculation of the averages
+  cumSumSignal = cumsum(signal);
+  cumSumSignal = [0 cumSumSignal];
+
+  # Denominator : mask part
+  meanMask = mean(mask)
+  denomMask = sqrt(sum((mask-meanMask).^2));
+
+  ans = zeros(1, length(signal)-length(mask)+1);
+
+  for d = 1:length(signal)-length(mask)+1
+      # Get the signal values for this slice/delay
+      sliceSignal = signal(d:d+length(mask)-1);
+      # Calculate the average of the slice (optimized)
+      avgSliceSignal = (cumSumSignal(d+length(mask))-cumSumSignal(d))/length(mask);
+      # Calculate the subtraction for faster computations
+      sliceMinAvg = sliceSignal - avgSliceSignal;
+      # The nominator
+      nom = sum( sliceMinAvg.* (mask-meanMask));
+      # Denominator : signal part
+      denomSignal = sqrt( sum( sliceMinAvg.^2 ) );
+      # The final answer
+      ans(d) = nom / (denomSignal*denomMask);
+  endfor
+  # Bye bye
