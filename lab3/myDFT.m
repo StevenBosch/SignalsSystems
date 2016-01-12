e5f81dbc28b31989531b2bf9ed0ada8e04b98e9a
Maleco <maikel.withagen@gmail.com>
2016-01-12 17:19:19 +0100

    bla

--- lab3/myDFT.m
+++ lab3/myDFT.m
@@ -1,12 +1,11 @@
 function[ans] = myDFT(x)
   N = length(x);
-  omega = exp((-2*pi*i)/N);
+  omega = exp((2*pi*i)/N);
   
   m = (0:N-1);
   VDM = ones(N, N);
-  for i = 2:N
+  for i = 1:N
     VDM(i,:) = omega .^ ((i-1) * m);
   endfor
-  VDM
   ans = (VDM * x')';
 end
