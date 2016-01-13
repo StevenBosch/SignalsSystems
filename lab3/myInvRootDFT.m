799070af78e4d8c92d7a10026ed1f5ef9b85b1d1
S1861948 <s1861948@fwn-bborg-edu-74-44.fmnsedu.rug.nl>
2016-01-13 11:58:04 +0100

    bla

--- lab3/myInvRootDFT.m
+++
@@ -1,8 +0,0 @@
-function[ans] = invNTT1(x)
-  N = length(x);
-  [root, prime] = rootsofunity(N)
-  rootInv = modinverse(root, prime)
-  
-  answers = VDM(rootInv, N) * x
-  ans = rem(modinverse(N, prime) * answers, prime);
-end
