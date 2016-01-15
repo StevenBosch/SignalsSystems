6f138b9287172c53844f7201ef8615c5d7caff14
S1861948 <s1861948@fwn-bborg-edu-74-4.fmnsedu.rug.nl>
2016-01-15 10:24:25 +0100

    bla

--- lab3/code/myconv.m
+++ lab3/code/myconv.m
@@ -1,6 +1,9 @@
 function myconv(x, y)
-  n = length(x);
+  % Pad both signals with the length of the other signal  
+  m = length(x);
+  n = length(y);
   x = [x zeros(1, n-1)];
-  y = [y zeros(1, n-1)];
+  y = [y zeros(1, m-1)];
+  % Calculate the convolution
   real(ifft(fft(x).*fft(y)))
 end
